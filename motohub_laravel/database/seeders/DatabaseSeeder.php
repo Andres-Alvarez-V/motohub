d62121a2b72890ff978235ba843833c10a038640
FelipeArredond <pipechiqui77@gmail.com>
2023-09-06 18:36:41 -0500

    Migrations done and relations fixed

--- motohub_laravel/database/seeders/DatabaseSeeder.php
+++ motohub_laravel/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
+use App\Models\Brand;
 use App\Models\Motorcycle;
 use Illuminate\Database\Seeder;
 
@@ -20,6 +21,7 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        Brand::factory(10)->create();   
         Motorcycle::factory(10)->create();
     }
 }
