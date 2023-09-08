4c53527f8676c24e83d3104e726350a59e014e68
Camilo Alvarez <camilo.alvarez@pragma.com.co>
2023-09-07 22:12:38 -0500

    Created migrations file User, factory and users model is done

--- motohub_laravel/app/Models/User.php
+++ motohub_laravel/app/Models/User.php
@@ -2,44 +2,109 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'name',
+        'birthDate',
+        'address',
         'email',
         'password',
+        'username',
+        'balance',
+        'role',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
-        'password',
-        'remember_token',
+        'password'
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
     protected $casts = [
-        'email_verified_at' => 'datetime',
-        'password' => 'hashed',
+        'birthDate' => 'date',
     ];
+
+    public function setName(string $name): void
+    {
+        $this->attributes['name'] = ucwords($name);
+    }
+
+    public function getName(): string
+    {
+        return $this->attributes['name'];
+    }
+
+    public function setBirthDate(string $birthDate): void
+    {
+        $this->attributes['birthDate'] = $birthDate;
+    }
+
+    public function getBirthDate(): string
+    {
+        return $this->attributes['birthDate'];
+    }
+
+    public function setAddress(string $address): void
+    {
+        $this->attributes['address'] = ucwords($address);
+    }
+
+    public function getAddress(): string
+    {
+        return $this->attributes['address'];
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->attributes['email'] = strtolower($email);
+    }
+
+    public function getEmail(): string
+    {
+        return $this->attributes['email'];
+    }
+
+    public function setPassword(string $password): void
+    {
+        $this->attributes['password'] = $password;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->attributes['password'];
+    }
+
+    public function setUsername(string $username): void
+    {
+        $this->attributes['username'] = strtolower($username);
+    }
+
+    public function getUsername(): string
+    {
+        return $this->attributes['username'];
+    }
+
+    public function setBalance(float $balance): void
+    {
+        $this->attributes['balance'] = $balance;
+    }
+
+    public function getBalance(): float
+    {
+        return $this->attributes['balance'];
+    }
+
+    public function setRole(string $role): void
+    {
+        $this->attributes['role'] = $role;
+    }
+
+    public function getRole(): string
+    {
+        return $this->attributes['role'];
+    }
 }
--- motohub_laravel/database/factories/UserFactory.php
+++ motohub_laravel/database/factories/UserFactory.php
@@ -20,19 +20,12 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
+            'birthDate' => fake()->date(),
+            'address' => fake()->address(),
+            'username' => fake()->userName(),
+            'balance' => fake()->randomNumber(7),
+            'role' => fake()->randomElement(['admin', 'user']),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
         ];
-    }
-
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
-    public function unverified(): static
-    {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
     }
 }
