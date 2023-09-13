ec61d3cd94001aed2f11740af37d3b1d946b30ea
FelipeArredond <pipechiqui77@gmail.com>
2023-09-12 20:29:04 -0500

    Hotfix

--- motohub_laravel/resources/views/user/brand/index.blade.php
+++ motohub_laravel/resources/views/user/brand/index.blade.php
@@ -2,7 +2,7 @@
 @section('title', trans('messages.brands'))
 @section('content')
 <div class="container">
-    <h1>Brands</h1>
+    <h1>{{ trans('messages.brands') }}</h1>
     <div class="row">
         @foreach ($viewData["brands"] as $brand)
         <div class="col-md-4 col-lg-3 mb-2 d-flex align-items-stretch">
@@ -15,7 +15,7 @@
                         src="{{$brand->getLogoImage()}}" class="card-img-top w-75"></div>
                 <div class="card-body item-card-inside-app">
                     <p class="card-text">{{$brand->getDescription()}}</p>
-                    <a href="{{ route('user.brand.show', ['id'=> $brand->getId()]) }}" class="btn btn-primary">{{ trans('message.show') }}</a>
+                    <a href="{{ route('user.brand.show', ['id'=> $brand->getId()]) }}" class="btn btn-primary">{{ trans('messages.show') }}</a>
                 </div>
                 <div class="card-footer text-center text-muted">
                     {{$brand->getFoundationYear()}}
--- motohub_laravel/resources/views/user/brand/show.blade.php
+++ motohub_laravel/resources/views/user/brand/show.blade.php
@@ -6,7 +6,7 @@
     <h1>
         {{ $viewData["brand"]->getName() }}
     </h1>
-    <img class="my-2" style="width: 200px; height: 150px" src="{{$viewData['logo_image']}}" alt="logo">
+    <img class="my-2" style="width: 200px; height: 150px" src="{{$viewData["brand"]->getLogoImage()}}" alt="logo">
     <p class="card-text my-2">{{ $viewData["brand"]->getDescription() }}</p>
     <ul class="list-group">
     @foreach ($viewData["brand"]->getMotorcycles() as $motorcycle)
