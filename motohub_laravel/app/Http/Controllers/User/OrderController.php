4187c2f0d872988dd7cd9283c2d4db96a22e6e3e
GianSz <giansz2003@gmail.com>
2023-09-13 10:42:06 -0500

    Changed order controller logic
    Handled possible malfunction when trying to buy motorcycles when their prices have been updated (subTotal was being stored in session and would have a different value than the real one)

--- motohub_laravel/app/Http/Controllers/User/OrderController.php
+++ motohub_laravel/app/Http/Controllers/User/OrderController.php
@@ -22,11 +22,22 @@ class OrderController extends Controller
     {
         $orderData = $request->session()->get('orderData');
         $orderItems = $orderData['orderItems'] ?? [];
-        $subTotal = $orderData['subTotal'] ?? 0;
 
+        $subTotal = 0;
+        $orderItemsView = [];
+        foreach($orderItems as $orderItem) {
+            $motorcycle = Motorcycle::findOrFail($orderItem['motorcycleId']);
+            $subTotal += $motorcycle->getPrice() * $orderItem['quantity'];
+
+            $orderItemsView[] = [
+                'motorcycle' => $motorcycle,
+                'quantity' => $orderItem['quantity'],
+            ];
+        }
+
+        $viewData['orderItems'] = $orderItemsView;
         $viewData['subTotal'] = $subTotal;
         $viewData['total'] = $subTotal + config('constants.SHIPPING_VALUE');
-        $viewData['orderItems'] = $orderItems;
         $viewData['shippingValue'] = config('constants.SHIPPING_VALUE');
 
         return view('user.order.index')->with('viewData', $viewData);
@@ -39,20 +50,14 @@ class OrderController extends Controller
         $motorcycleId = $request->input('motorcycle_id');
         $quantity = $request->input('quantity');
 
-        $motorcycle = Motorcycle::findOrFail($motorcycleId)->toArray();
-
         $orderData = $request->session()->get('orderData');
         $orderItems = $orderData['orderItems'] ?? [];
-        $subTotal = $orderData['subTotal'] ?? 0;
 
         $orderItems[] = [
-            'motorcycle' => $motorcycle,
+            'motorcycleId' => $motorcycleId,
             'quantity' => $quantity,
         ];
-
         $orderData['orderItems'] = $orderItems;
-        $orderData['subTotal'] = $subTotal + $motorcycle['price'] * $quantity;
-
         $request->session()->put('orderData', $orderData);
 
         return to_route('order.index');
@@ -61,10 +66,7 @@ class OrderController extends Controller
     public function delete(int $id, Request $request): RedirectResponse
     {
         $orderData = $request->session()->get('orderData');
-        $orderItems = $orderData['orderItems'] ?? [];
-        $subTotal = $orderData['subTotal'] ?? 0;
 
-        $orderData['subTotal'] = $subTotal - $orderItems[$id]['motorcycle']['price'] * $orderItems[$id]['quantity'];
         unset($orderData['orderItems'][$id]);
 
         $request->session()->put('orderData', $orderData);
@@ -81,23 +83,31 @@ class OrderController extends Controller
 
     public function save(Request $request): RedirectResponse|View
     {
+        Order::validateSave($request);
+
         $orderData = $request->session()->get('orderData');
         $orderItems = $orderData['orderItems'] ?? [];
-        $subTotal = $orderData['subTotal'] ?? 0;
-
-        Order::validateSave($request);
 
         $userId = auth()->user()->id;
         $user = User::findOrFail($userId);
-
         $userBalance = $user->getBalance();
 
-        if ($userBalance < $subTotal + config('constants.SHIPPING_VALUE')) {
+        $subTotal = 0;
+        foreach($orderItems as $orderItem) {
+            $motorcycle = Motorcycle::findOrFail($orderItem['motorcycleId']);
+            $motorcycle->update([
+                'stock' => $motorcycle->getStock() - $orderItem['quantity'],
+            ]);
+            $subTotal += $motorcycle->getPrice() * $orderItem['quantity'];
+        }
+        $total = $subTotal + config('constants.SHIPPING_VALUE');
+
+        if ($userBalance < $total) {
             return back()->withErrors(['message' => trans('messages.orderInsufficientFunds')]);
         }
 
         $user->update([
-            'balance' => $userBalance - ($subTotal + config('constants.SHIPPING_VALUE')),
+            'balance' => $userBalance - $total,
         ]);
 
         $order = Order::create([
@@ -106,15 +116,14 @@ class OrderController extends Controller
             'shipping_address' => $request->input('shipping_address') ?? $user->getAddress(),
             'shipping_value' => config('constants.SHIPPING_VALUE'),
             'sub_total' => $subTotal,
-            'total' => $subTotal + config('constants.SHIPPING_VALUE'),
+            'total' => $total,
             'active' => false,
         ]);
 
-        //Get all the order items stored in the session
         foreach ($orderItems as $orderItem) {
             OrderItem::create([
                 'order_id' => $order->getId(),
-                'motorcycle_id' => $orderItem['motorcycle']['id'],
+                'motorcycle_id' => $orderItem['motorcycleId'],
                 'quantity' => $orderItem['quantity'],
             ]);
         }
