dd246d1f2b607e75c4b00ca48e3f69d679778883
GianSz <giansz2003@gmail.com>
2023-09-19 14:01:22 -0500

    Changes in order's logic.
    Order logic was presenting a problem since we were substracting a quantity from the motocycle stock before we knew if the user could pay the total amount of the order. Also, there was another problem with adding the same motorcycle to the cart but in different instances, since we were not validating that the sum of the quantities of this same motorcycle was less than the stock of it.

--- motohub_laravel/app/Http/Controllers/User/OrderController.php
+++ motohub_laravel/app/Http/Controllers/User/OrderController.php
@@ -7,6 +7,7 @@ use App\Models\Motorcycle;
 use App\Models\Order;
 use App\Models\OrderItem;
 use App\Models\User;
+use Illuminate\Database\QueryException;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
@@ -53,6 +54,18 @@ class OrderController extends Controller
         $orderData = $request->session()->get('orderData');
         $orderItems = $orderData['orderItems'] ?? [];
 
+        foreach($orderItems as $key => $orderItem){
+            if ($orderItem['motorcycleId'] == $motorcycleId) {
+                $orderItem['quantity'] += $quantity;
+
+                $orderItems[$key] = $orderItem;
+                $orderData['orderItems'] = $orderItems;
+
+                $request->session()->put('orderData', $orderData);
+                return to_route('order.index');
+            }
+        }
+
         $orderItems[] = [
             'motorcycleId' => $motorcycleId,
             'quantity' => $quantity,
@@ -83,52 +96,60 @@ class OrderController extends Controller
 
     public function save(Request $request): RedirectResponse|View
     {
-        Order::validateSave($request);
+        try{
+            Order::validateSave($request);
 
-        $orderData = $request->session()->get('orderData');
-        $orderItems = $orderData['orderItems'] ?? [];
+            $orderData = $request->session()->get('orderData');
+            $orderItems = $orderData['orderItems'] ?? [];
 
-        $userId = auth()->user()->id;
-        $user = User::findOrFail($userId);
-        $userBalance = $user->getBalance();
+            $subTotal = 0;
+            $motorcycles = [];
+            foreach($orderItems as $orderItem) {
+                $motorcycle = Motorcycle::findOrFail($orderItem['motorcycleId']);
+                $motorcycles[] = $motorcycle;
+                $subTotal += $motorcycle->getPrice() * $orderItem['quantity'];
+            }
+            $total = $subTotal + config('constants.SHIPPING_VALUE');
 
-        $subTotal = 0;
-        foreach($orderItems as $orderItem) {
-            $motorcycle = Motorcycle::findOrFail($orderItem['motorcycleId']);
-            $motorcycle->update([
-                'stock' => $motorcycle->getStock() - $orderItem['quantity'],
+            $userId = auth()->user()->id;
+            $user = User::findOrFail($userId);
+            $userBalance = $user->getBalance();
+            if ($userBalance < $total) {
+                return back()->withErrors(['message' => trans('messages.orderInsufficientFunds')]);
+            }
+
+            foreach($motorcycles as $motorcycle) {
+                $motorcycle->update([
+                    'stock' => $motorcycle->getStock() - $orderItem['quantity'],
+                ]);
+            }
+
+            $user->update([
+                'balance' => $userBalance - $total,
             ]);
-            $subTotal += $motorcycle->getPrice() * $orderItem['quantity'];
-        }
-        $total = $subTotal + config('constants.SHIPPING_VALUE');
 
-        if ($userBalance < $total) {
-            return back()->withErrors(['message' => trans('messages.orderInsufficientFunds')]);
-        }
-
-        $user->update([
-            'balance' => $userBalance - $total,
-        ]);
-
-        $order = Order::create([
-            'user_id' => $userId,
-            'payment' => 'approved',
-            'shipping_address' => $request->input('shipping_address') ?? $user->getAddress(),
-            'shipping_value' => config('constants.SHIPPING_VALUE'),
-            'sub_total' => $subTotal,
-            'total' => $total,
-            'active' => false,
-        ]);
-
-        foreach ($orderItems as $orderItem) {
-            OrderItem::create([
-                'order_id' => $order->getId(),
-                'motorcycle_id' => $orderItem['motorcycleId'],
-                'quantity' => $orderItem['quantity'],
+            $order = Order::create([
+                'user_id' => $userId,
+                'payment' => 'approved',
+                'shipping_address' => $request->input('shipping_address') ?? $user->getAddress(),
+                'shipping_value' => config('constants.SHIPPING_VALUE'),
+                'sub_total' => $subTotal,
+                'total' => $total,
+                'active' => false,
             ]);
-        }
-        $request->session()->forget('orderData');
 
-        return view('user.order.save');
+            foreach ($orderItems as $orderItem) {
+                OrderItem::create([
+                    'order_id' => $order->getId(),
+                    'motorcycle_id' => $orderItem['motorcycleId'],
+                    'quantity' => $orderItem['quantity'],
+                ]);
+            }
+            $request->session()->forget('orderData');
+
+            return view('user.order.save');
+        } catch (QueryException $e) {
+            return back()->withErrors(['message' => trans('messages.exceededStock')]);
+        }
     }
 }
