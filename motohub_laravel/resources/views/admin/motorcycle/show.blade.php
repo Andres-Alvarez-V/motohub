8bdbe5050bd5f864547d67c051500b9f8b4abd18
GianSz <giansz2003@gmail.com>
2023-09-19 13:53:02 -0500

    Modified multiple views to make them look better.
    Developed a component with the detail of the brand and other similar component with the detail of the motorcycle, then included these componentes in all of the 'show' views.
    
    Included the search bar component in the admin views.
    
    Modified the navbar by adding a nav item that redirects to the shopping cart (at this moment there was no way to get into this module).

--- motohub_laravel/resources/views/admin/motorcycle/show.blade.php
+++ motohub_laravel/resources/views/admin/motorcycle/show.blade.php
@@ -3,17 +3,7 @@
 @section('subtitle', $viewData["motorcycle"]->getName())
 @section('content')
 <div class="container">
-    <h1>
-        {{ $viewData["motorcycle"]->getName() }}
-    </h1>
-    <img class="my-2" style="width: 200px; height: 150px" src="{{ URL::asset('storage/' . $viewData["motorcycle"]->getImage()) }}" alt="logo">
-    <p class="card-text my-2">{{ $viewData["motorcycle"]->getDescription() }}</p>
-    <p class="card-text my-2">Stock: {{ $viewData["motorcycle"]->getStock() }}</p>
-    <p class="card-text my-2">${{ $viewData["motorcycle"]->getPrice() }}</p>
-    @if($viewData["motorcycle"]->getIsActive())
-        <a href="{{ route( 'admin.motorcycle.disable' , ['id' =>$viewData["motorcycle"]->getId()]) }}" class="btn btn-primary-app text-white my-2">{{trans('messages.disableForSale')}}</a>
-    @else
-        <a href="{{ route( 'admin.motorcycle.enable' , ['id' =>$viewData["motorcycle"]->getId()]) }}" class="btn btn-primary-app text-white my-2">{{trans('messages.enableForSale')}}</a>
-    @endif
+    <h1 class="text-center my-3">{{ trans('messages.motoInfo') }}</h1>
+    @include('includes.infoMotorcycle')
 </div>
 @endsection
