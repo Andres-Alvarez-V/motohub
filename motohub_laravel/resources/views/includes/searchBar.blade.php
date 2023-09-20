0285d4b1f871d5917acd5c1da74c79e382b01fa4
FelipeArredond <pipechiqui77@gmail.com>
2023-09-19 22:16:31 -0500

    Search bar comoponent improved

--- motohub_laravel/resources/views/includes/searchBar.blade.php
+++ motohub_laravel/resources/views/includes/searchBar.blade.php
@@ -4,9 +4,11 @@
         <input type="text" class="form-control search-bar-app" name="search" placeholder="{{ trans('messages.search') }}..."/>
         <button type="submit" class="btn btn-primary-app">{{ trans('messages.search') }}</button>
     </div>
-    <select class="form-select w-25 m-auto mt-2 filter-app" name="sortBy">
-        <option value="" selected>No filter</option>
-        <option value="desc">Descending price</option>
-        <option value="asc">Ascending price</option>
-    </select>
+    @if(Route::currentRouteName() == 'user.motorcycle.index' || Route::currentRouteName() == 'admin.motorcycle.index')
+        <select class="form-select w-25 m-auto mt-2 filter-app" name="sortBy">
+            <option value="" selected>No filter</option>
+            <option value="desc">Descending price</option>
+            <option value="asc">Ascending price</option>
+        </select>
+    @endif
 </form>
