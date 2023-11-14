c119127cb4cc7c0ca83024fe823eb35e6821e707
FelipeArredond <pipechiqui77@gmail.com>
2023-11-14 07:52:39 -0500

    Third part api integrated and workflows using gh secrets

--- motohub_laravel/app/Http/Controllers/Admin/MotorcycleController.php
+++ motohub_laravel/app/Http/Controllers/Admin/MotorcycleController.php
@@ -7,6 +7,7 @@ use App\Interfaces\ImageStorage;
 use App\Models\Brand;
 use App\Models\Motorcycle;
 use App\Models\State;
+use GuzzleHttp\Client;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
@@ -42,8 +43,31 @@ class MotorcycleController extends Controller
 
     public function show(int $id): View
     {
+
+        $client = new Client();
+        $url = "https://api.api-ninjas.com/v1/motorcycles";
+
         $motorcycle = Motorcycle::findOrFail($id);
         $viewData['motorcycle'] = $motorcycle;
+
+        $apiData = $client->request('GET', $url, [
+            'query'=> ['make' => $motorcycle->brand->name, 'model' => $motorcycle->name],
+            'headers' => ['X-Api-key' => 'a6uay7hV8+Yqy3tj+yVf4A==KZZ4psqDm4ZJINBk']
+        ]);
+
+        $contentsData = json_decode($apiData->getBody()->getContents(), true);
+
+        $viewData['displacement'] = $contentsData[0]['displacement'];
+        $viewData['engine'] = $contentsData[0]['engine'];
+        $viewData['power'] = $contentsData[0]['power'];
+        $viewData['torque'] = $contentsData[0]['torque'];
+        $viewData['valvesPerCylinder'] = $contentsData[0]['valves_per_cylinder'];
+        $viewData['fuelSystem'] = $contentsData[0]['fuel_system'];
+        $viewData['cooling'] = $contentsData[0]['cooling'];
+        $viewData['frontTire'] = $contentsData[0]['front_tire'];
+        $viewData['rearTire'] = $contentsData[0]['rear_tire'];
+        $viewData['fuelCapacity'] = $contentsData[0]['fuel_capacity'];
+        $viewData['totalWeight'] = $contentsData[0]['total_weight'];
 
         return view('admin.motorcycle.show')->with('viewData', $viewData);
     }
