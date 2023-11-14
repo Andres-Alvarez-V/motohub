9a48ff1300c048bc2aee90275eb3791288da4193
FelipeArredond <pipechiqui77@gmail.com>
2023-11-13 23:54:04 -0500

    Updated HTTP unit tests

--- motohub_laravel/tests/Feature/MotorcycleTest.php
+++ motohub_laravel/tests/Feature/MotorcycleTest.php
@@ -10,7 +10,12 @@ class MotorcycleTest extends TestCase
 
     public function test_admin_motorcycles(): void
     {
-        $user = User::factory()->create();
+        $user = new User();
+        $user->setAddress("test_addres");
+        $user->setName("test_name");
+        $user->setEmail("test@email.test");
+        $user->setPassword("$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi");//12345678
+        $user->setRole("ADMIN");
 
         $response = $this->actingAs($user)->get('/admin/motorcycles');
 
