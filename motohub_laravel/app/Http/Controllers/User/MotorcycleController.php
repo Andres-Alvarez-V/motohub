4fc5ac2b703b0a2533223f7b6436a445c7a41893
FelipeArredond <pipechiqui77@gmail.com>
2023-11-14 07:53:18 -0500

    Added Thir part api data to both roles view

--- motohub_laravel/app/Http/Controllers/User/MotorcycleController.php
+++ motohub_laravel/app/Http/Controllers/User/MotorcycleController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use App\Models\Motorcycle;
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
@@ -38,9 +39,32 @@ class MotorcycleController extends Controller
 
     public function show(int $id): View
     {
-        $motorcycle = Motorcycle::where('is_active', true)->findOrFail($id);
+
+        $client = new Client();
+        $url = "https://api.api-ninjas.com/v1/motorcycles";
+
+        $motorcycle = Motorcycle::findOrFail($id);
         $viewData['motorcycle'] = $motorcycle;
 
-        return view('user.motorcycle.show')->with('viewData', $viewData);
+        $apiData = $client->request('GET', $url, [
+            'query'=> ['make' => $motorcycle->brand->name, 'model' => $motorcycle->name],
+            'headers' => ['X-Api-key' => 'a6uay7hV8+Yqy3tj+yVf4A==KZZ4psqDm4ZJINBk']
+        ]);
+
+        $contentsData = json_decode($apiData->getBody()->getContents(), true);
+
+        $viewData['displacement'] = $contentsData[0]['displacement'];
+        $viewData['engine'] = $contentsData[0]['engine'];
+        $viewData['power'] = $contentsData[0]['power'];
+        $viewData['torque'] = $contentsData[0]['torque'];
+        $viewData['valvesPerCylinder'] = $contentsData[0]['valves_per_cylinder'];
+        $viewData['fuelSystem'] = $contentsData[0]['fuel_system'];
+        $viewData['cooling'] = $contentsData[0]['cooling'];
+        $viewData['frontTire'] = $contentsData[0]['front_tire'];
+        $viewData['rearTire'] = $contentsData[0]['rear_tire'];
+        $viewData['fuelCapacity'] = $contentsData[0]['fuel_capacity'];
+        $viewData['totalWeight'] = $contentsData[0]['total_weight'];
+
+        return view('admin.motorcycle.show')->with('viewData', $viewData);
     }
 }
