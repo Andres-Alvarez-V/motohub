73630744c0edd8753714b2289799aad606d56f4a
Camilo Alvarez <camilo.alvarez@pragma.com.co>
2023-11-15 00:05:34 -0500

    estandarizacion visual de formularios

--- motohub_laravel/resources/views/admin/motorcycle/create.blade.php
+++ motohub_laravel/resources/views/admin/motorcycle/create.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.app')
 @section('title', '- '.trans('messages.motorcycles'))
 @section('content')
-<div class="container">
+<div class="container mt-5">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
+        <div class="col-md-8 card-container-app">
+            <div class="card bg-secondary-color-app text-white">
                 <div class="card-header">{{ trans('messages.motorcycles') }}</div>
                 <div class="card-body">
                     @if($errors->any())
@@ -16,11 +16,11 @@
                     @endif
                     <form method="POST" action="{{ route('admin.motorcycle.save') }}" enctype="multipart/form-data">
                         @csrf
-                        <input type="text" class="form-control mb-2" placeholder="Enter name" name="name" value="{{ old('name') }}" />
-                        <input type="text" class="form-control mb-2" placeholder="Enter model" name="model" value="{{ old('model') }}" />
-                        <input type="text" class="form-control mb-2" placeholder="Enter category" name="category" value="{{ old('category') }}" />
-                        <input type="file" class="form-control mb-2" name="image" />
-                        <input type="text" class="form-control mb-2" placeholder="Enter a description" name="description" value="{{ old('description') }}" />
+                        <input type="text" class="input-app text-white mb-2" placeholder="Enter name" name="name" value="{{ old('name') }}" />
+                        <input type="text" class="input-app text-white mb-2" placeholder="Enter model" name="model" value="{{ old('model') }}" />
+                        <input type="text" class="input-app text-white mb-2" placeholder="Enter category" name="category" value="{{ old('category') }}" />
+                        <input type="file" class="input-app text-white mb-2" name="image" />
+                        <input type="text" class="input-app text-white mb-2" placeholder="Enter a description" name="description" value="{{ old('description') }}" />
                         <div style="display:flex;flex-direction:column;margin:10px 0">
                             <label for="brand_id">Brand</label>
                             <select name="brand_id">
@@ -29,8 +29,8 @@
                                 @endforeach
                             </select>
                         </div>
-                        <input type="number" class="form-control mb-2" placeholder="Enter a stock" name="stock" value="{{ old('stock') }}" />
-                        <input type="number" class="form-control mb-2" placeholder="Enter a price" name="price" value="{{ old('price') }}" />
+                        <input type="number" class="input-app text-white mb-2" placeholder="Enter a stock" name="stock" value="{{ old('stock') }}" />
+                        <input type="number" class="input-app text-white mb-2" placeholder="Enter a price" name="price" value="{{ old('price') }}" />
                         <input type="submit" class="btn btn-primary-app" value="{{ trans('messages.send') }}" />
                     </form>
                 </div>
