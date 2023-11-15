8e84e7defc927f7ad1803f377b5ce4ca5cb8f566
GianSz <giansz2003@gmail.com>
2023-11-15 02:19:02 -0500

    Changed admin home page
    Added message about our page

--- motohub_laravel/resources/views/admin/home.blade.php
+++ motohub_laravel/resources/views/admin/home.blade.php
@@ -3,20 +3,11 @@
 @section('content')
 <div class="container">
     <div class="row mt-3">
-        <div class="col-md-8 m-auto">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are an admin and are logged in!') }}
-                </div>
-            </div>
+        <div class="col-md-8 m-auto text-center">
+            <h3>Motohub</h3>
+            <p class="text-white">
+                {{ trans('messages.homeInfo') }}
+            </p>
         </div>
     </div>
 </div>
