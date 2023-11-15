74f658dbb6c0088acbdfc23cb61d0556968643f3
FelipeArredond <pipechiqui77@gmail.com>
2023-11-15 14:36:49 -0500

    Added resources

--- motohub_laravel/app/Http/Controllers/Api/MotorcycleApiController.php
+++ motohub_laravel/app/Http/Controllers/Api/MotorcycleApiController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\MotorcycleResource;
 use App\Models\Motorcycle;
 use Illuminate\Http\JsonResponse;
 
@@ -11,14 +12,14 @@ class MotorcycleApiController extends Controller
 
     public function index(): JsonResponse
     {
-        $motorcycle = Motorcycle::all();
+        $motorcycle = MotorcycleResource::collection(Motorcycle::all());
 
         return response()->json($motorcycle, 200);
     }
 
     public function show(string $motorcycleId): JsonResponse
     {
-        $motorcycles = Motorcycle::findOrFail( $motorcycleId );
+        $motorcycles = new MotorcycleResource(Motorcycle::findOrFail($motorcycleId));
 
         return response()->json($motorcycles, 200);
     }
