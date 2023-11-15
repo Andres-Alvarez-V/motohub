dee057aa660029691b80de52b403e8de33403a6a
FelipeArredond <pipechiqui77@gmail.com>
2023-11-14 20:40:19 -0500

    Updated unit tests

--- motohub_laravel/tests/Feature/AnalyticsTest.php
+++ motohub_laravel/tests/Feature/AnalyticsTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Http\Controllers\Admin\AnalyticsController;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -10,18 +11,12 @@ use Tests\TestCase;
 class AnalyticsTest extends TestCase
 {
     
-    public function test_admin_analytics(): void
+    public function testIndex()
     {
-        $user = new User();
-        $user->setAddress("test_addres");
-        $user->setName("test_name");
-        $user->setEmail("test@email.test");
-        $user->setPassword("$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi");//12345678
-        $user->setRole("ADMIN");
+        $controller = new AnalyticsController();
+        $view = $controller->index();
 
-        $response = $this->actingAs($user)->get('/admin/analytics');
-
-        $response->assertStatus(200);
+        $this->assertEquals('admin.analytics.index', $view->getName());
     }
 
 }
